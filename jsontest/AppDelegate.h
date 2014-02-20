f16157edb9243b106b1aa358cd014124e531ca66
Daniel Hoy <studentadmin3@EmmetMcFaddens-MacBook-Pro.local>
2014-02-20 10:44:28 +0000

    Initial Commit

---
+++ jsontest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  jsontest
+//
+//  Created by Daniel Hoy on 20/02/2014.
+//  Copyright (c) 2014 Daniel Hoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
