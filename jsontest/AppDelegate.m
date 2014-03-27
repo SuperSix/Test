155c2897cb0d142210b51eeee6be955c1eb6b8f0
SuperSix <jamessreenan@live.ie>
2014-03-27 15:35:26 +0000

    Test2

--- jsontest/AppDelegate.m
+++ jsontest/AppDelegate.m
@@ -12,6 +12,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // Override point for customization after application launch.
+    NSLog(@"application did finish launching");
     return YES;
 }
 
